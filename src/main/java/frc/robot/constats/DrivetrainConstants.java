68c05d49b2b99ac1b2df0f2bfe22509744ce907f
Mark-Lua <s62711@parkhill.k12.mo.us>
2025-01-16 18:12:51 -0600

    Used Proper Constant Declration
    Co-authored-by: Greg Howdeshell <poloka@users.noreply.github.com>

--- src/main/java/frc/robot/constats/DrivetrainConstants.java
+++ src/main/java/frc/robot/constats/DrivetrainConstants.java
@@ -13,15 +13,13 @@ import frc.robot.generated.TunerConstants;
 
 /** Add your docs here. */
 public class DrivetrainConstants {
-    public static final double maxSpeedMetersPerSecond = TunerConstants.kSpeedAt12VoltsMps;
-    public static final double trackWidthMeters = Units.inchesToMeters(18.78);
-    public static final double rotationDiameter = trackWidthMeters * Math.PI * Math.sqrt(2);
-    public static final double rotationsPerSecond = maxSpeedMetersPerSecond / rotationDiameter;
-    public static final double maxAngularVelocityRadiansPerSecond = 2 * Math.PI * rotationsPerSecond;
-    public static final double maxAngularSpeed = 2 * Math.PI;
-    public static Matrix<N3, N1> visionMeasurementStdDevs = VecBuilder.fill(0.9, 0.9, 0.9);
-    public static final double currentLimit = 60;
-    public static final double autoMaxSpeedMetersPerSecond = maxSpeedMetersPerSecond * 0.8;
-    public static final double estimatedKp = 12/(maxSpeedMetersPerSecond/ (Units.inchesToMeters(TunerConstants.kWheelRadiusInches) * 2 * Math.PI));
-
+  public static final double maxSpeedMetersPerSecond = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond);
+  public static final double trackWidthMeters = Units.inchesToMeters(18.78);
+  public static final double rotationDiameter = trackWidthMeters * Math.PI * Math.sqrt(2);
+  public static final double rotationsPerSecond = maxSpeedMetersPerSecond / rotationDiameter;
+  public static final double maxAngularVelocityRadiansPerSecond = 2 * Math.PI * rotationsPerSecond;
+  public static Matrix<N3, N1> visionMeasurementStdDevs = VecBuilder.fill(0.9, 0.9, 0.9);
+  public static final double currentLimit = 60;
+  public static final double autoMaxSpeedMetersPerSecond = maxSpeedMetersPerSecond * 0.8;
+  public static final double estimatedKp = 12/(maxSpeedMetersPerSecond/ (TunerConstants.kWheelRadius.in(Meters) * 2 * Math.PI));
 }
