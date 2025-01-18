f747c12a7f848223587a573d6c64ff36001bf56b
28anvbha <s86769@parkhill.k12.mo.us>
2025-01-18 15:18:52 -0600

    Working on April Tag Localization, Coordinates on the Field.

--- src/main/java/frc/robot/vision/AprilTagLocalization.java
+++ src/main/java/frc/robot/vision/AprilTagLocalization.java
@@ -12,6 +12,7 @@ import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.wpilibj.Notifier;
 import frc.robot.vision.AprilTagLocalizationConstants.LimelightDetails;
+import frc.robot.vision.LimelightHelpers.PoseEstimate;
 
 
 /**
@@ -23,6 +24,12 @@ public class AprilTagLocalization {
   private LimelightDetails[] m_LimelightDetails;  // list of limelights that can provide updates
   private Supplier<Pose2d> m_robotPoseSupplier;  // supplies the pose of the robot
   private boolean fullTrust;  //to allow for button trust the tag estimate over all else.
+  private double oldyawDegrees = 0;
+ 
+  
+  private double maxFieldDistanceX = 0.0;
+  private double maxFieldDistanceY = 0.0;
+
 
   /**
    * Creates a new AprilTagLocalization.
@@ -32,7 +39,7 @@ public class AprilTagLocalization {
    */
   public AprilTagLocalization(Supplier<Pose2d> poseSupplier, VisionConsumer visionConsumer,  LimelightDetails ... details) {
     m_notifier.startPeriodic(0.02); // set up a pose estimation loop with a 0.02 second period.
-    m_LimelightDetails= details;
+    m_LimelightDetails = details;
     m_robotPoseSupplier = poseSupplier;
   }
 
@@ -43,23 +50,40 @@ public class AprilTagLocalization {
   public void setFullTrust(boolean fullTrust) {
     this.fullTrust = fullTrust;
   }
-
-  public void poseEstimate() {
-    for (LimelightDetails limelightDetail : m_LimelightDetails) {
-      // Set Orientation using LimelightHelpers.SetRobotOrientation and the m_robotPoseSupplier
-      // Get the pose from the Limelight
-      // Validate the pose for sanity reject bad poses  if fullTrust is true accept regarless of sanity
-      // reject poses that are more than max tag distance we trust
-      // scale std deviation by distance if fullTrust is true set the stdDevs super low.
-      // set the pose in the pose consumer
+  private boolean isPoseOnfield(Pose2d observation) { 
+    // Coordinates for where Pose is on the field
+    if (
+      observation.getX() < 0.0 // What X position robot is on the field.
+      || observation.getY() < 0.0 // What Y position robot is on the field.
+      || observation.getX() > maxFieldDistanceX // Whether the robot X position is on the field or not
+      || observation.getY() > maxFieldDistanceY // Whether the robot X position is on the field or not
+    ){
+      return true;
+    } else {
+      return false;
     }
+  }
+
+  public Pose2d poseEstimate() {
+      for (LimelightDetails limelightDetail : m_LimelightDetails) {
+        double yawDegrees = m_robotPoseSupplier.get().getRotation().getDegrees();
+        double yawRateDegreesPerSecond = (yawDegrees-oldyawDegrees)/0.02;
+        LimelightHelpers.SetRobotOrientation(limelightDetail.name, yawDegrees, yawRateDegreesPerSecond,0,0,0,0 );  // Set Orientation using LimelightHelpers.SetRobotOrientation and the m_robotPoseSupplier
+        PoseEstimate poseEstimate = LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2(null);  // Get the pose from the Limelight 
+        if (isPoseOnfield(poseEstimate.pose)){
+          return poseEstimate.pose;
+        }
+        // Validates the pose for sanity reject bad poses if fullTrust is true accept regarless of sanity
+        // rejects poses that are more than max tag distance we trust
+      }
+      return null; // return null if no valid pose is found
   }
   
   @FunctionalInterface
   public interface VisionConsumer {
     void accept(
-        Pose2d visionRobotPoseMeters,
-        double timestampSeconds,
-        Matrix<N3, N1> visionMeasurementStdDevs);
+      Pose2d visionRobotPoseMeters,
+      double timestampSeconds,
+      Matrix<N3, N1> visionMeasurementStdDevs);
   }
 }
