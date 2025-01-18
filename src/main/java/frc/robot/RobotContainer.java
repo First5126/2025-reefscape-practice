d88c55c002027f3bf577c6d65c6facc5fad08e52
Mark-Lua <s62711@parkhill.k12.mo.us>
2025-01-18 11:20:08 -0600

    Add Auto Systems (#14)
    Co-authored-by: RoDev09 <robotwizard09@gmail.com>
    Co-authored-by: Greg Howdeshell <poloka@users.noreply.github.com>

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,8 +8,12 @@ import static edu.wpi.first.units.Units.*;
 
 import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
 import com.ctre.phoenix6.swerve.SwerveRequest;
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.commands.PathPlannerAuto;
 
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -19,46 +23,42 @@ import frc.robot.subsystems.CommandSwerveDrivetrain;
 
 public class RobotContainer {
   private final CommandSwerveDrivetrain m_drivetrain = TunerConstants.DriveTrain;
+    private double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
+    private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
 
-  private final CommandXboxController m_driver_controller = new CommandXboxController(0);
-  private final CommandXboxController m_codriver_controller = new CommandXboxController(1);
+    /* Setting up bindings for necessary control of the swerve drive platform*/
+    private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
+            .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
+            .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive motors
+    private final SwerveRequest.RobotCentric driveRobCentric = new SwerveRequest.RobotCentric()
+            .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
+            .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive motors
+    private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
+    private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
+    private final CommandXboxController m_driver_controller = new CommandXboxController(0);
+    private final CommandXboxController m_codriver_controller = new CommandXboxController(1);
+    private final SendableChooser<Command> autoChooser = AutoBuilder.buildAutoChooser();
+    
+    private final CommandXboxController m_driverController = new CommandXboxController(0);
 
-  private double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
-  private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
+    public RobotContainer() {
+        configureBindings();
+        configureCoDriverControls();
 
-  /* Setting up bindings for necessary control of the swerve drive platform*/
-  private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
-    .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
-    .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive motors
-  private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
-  private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
+        // Adds a auto chooser to Shuffle Board to choose autos
+        SmartDashboard.putData("Auto Chooser", autoChooser);
+    }
 
-  private final Telemetry logger = new Telemetry(MaxSpeed);
-
-  public RobotContainer() {
-    configureDriverControls();
-    configureCoDriverControls();
-  }
-
-  private void configureDriverControls() {
-    // Setup drivetrain default command.  Should this be elsewhere???
-    // Note that X is defined as forward according to WPILib convention,
-    // and Y is defined as to the left according to WPILib convention.
-    m_drivetrain.setDefaultCommand(
-        // Drivetrain will execute this command periodically
-        m_drivetrain.applyRequest(() ->
-            drive.withVelocityX(-m_driver_controller.getLeftY() * MaxSpeed) // Drive forward with negative Y (forward)
-                .withVelocityY(-m_driver_controller.getLeftX() * MaxSpeed) // Drive left with negative X (left)
-                .withRotationalRate(-m_driver_controller.getRightX() * MaxAngularRate) // Drive counterclockwise with negative X (left)
-        )
-    );
-    m_drivetrain.registerTelemetry(logger::telemeterize);
-
-    // Setup driver's controlls
-    m_driver_controller.a().whileTrue(m_drivetrain.applyRequest(() -> brake));
-    m_driver_controller.b().whileTrue(m_drivetrain.applyRequest(() ->
-        point.withModuleDirection(new Rotation2d(-m_driver_controller.getLeftY(), -m_driver_controller.getLeftX()))
-    ));
+    private void configureBindings() {
+        // Note that X is defined as forward according to WPILib convention,
+        // and Y is defined as to the left according to WPILib convention.
+        
+        m_drivetrain.setDefaultCommand(m_drivetrain.gasPedalCommand(
+            m_driverController::getRightTriggerAxis,
+            m_driverController::getRightX,
+            m_driverController::getLeftY,
+            m_driverController::getLeftX
+        ));
 
     m_drivetrain.setDefaultCommand(
         m_drivetrain.gasPedalCommand(
@@ -75,6 +75,6 @@ public class RobotContainer {
   }
 
   public Command getAutonomousCommand() {
-    return Commands.print("No autonomous command configured");
+    return autoChooser.getSelected();
   }
 }
