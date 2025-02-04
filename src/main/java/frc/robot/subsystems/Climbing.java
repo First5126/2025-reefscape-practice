cacdf9ed79194ec60656ee4e29fd0b8918c95c74
Mark-Lua <s62711@parkhill.k12.mo.us>
2025-02-04 16:07:24 -0600

    Added Climbing Subsystem

---
+++ src/main/java/frc/robot/subsystems/Climbing.java
@@ -0,0 +1,46 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.controls.ControlRequest;
+import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.constants.ClimbingConstants;
+
+public class Climbing extends SubsystemBase {
+
+  private final TalonFX m_leftMotor = new TalonFX(0);
+  private final TalonFX m_rightMotor = new TalonFX(1);
+  private final PositionVoltage m_PositionVoltage = new PositionVoltage(0).withSlot(0).withFeedForward(0);
+
+  public Climbing() {
+    m_rightMotor.setControl(new Follower(m_leftMotor.getDeviceID(), true));
+
+    unClimb();
+  }
+
+  // TODO: add the correct constant for the position
+  public Command climb() {
+    return run(() -> {setControl(m_PositionVoltage.withPosition(ClimbingConstants.ROTATIONS_FOR_CLIMB));});
+  }
+
+  public Command unClimb() {
+    return run(() -> {setControl(m_PositionVoltage.withPosition(0));});
+  }
+
+  public void setControl(ControlRequest control){
+    m_leftMotor.setControl(control);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
