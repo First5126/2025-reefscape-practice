955a72ca7ee60b9cf74afc6cda6957eaa1cacbc3
Mark-Lua <s62711@parkhill.k12.mo.us>
2025-02-04 18:26:28 -0600

    Climbing digital input (#34)

--- src/main/java/frc/robot/constants/ClimbingConstants.java
+++ src/main/java/frc/robot/constants/ClimbingConstants.java
@@ -1,5 +1,6 @@
 package frc.robot.constants;
 
 public class ClimbingConstants {
+    // The amount of rotations required for the robot to enter the climbed state
     public static final double ROTATIONS_FOR_CLIMB = 0;
 }
--- src/main/java/frc/robot/subsystems/Climbing.java
+++ src/main/java/frc/robot/subsystems/Climbing.java
@@ -1,10 +1,16 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.configs.HardwareLimitSwitchConfigs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.ControlRequest;
+import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.controls.TorqueCurrentFOC;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.ForwardLimitSourceValue;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.ClimbingConstants;
@@ -15,16 +21,23 @@ public class Climbing extends SubsystemBase {
   private final TalonFX m_rightMotor = new TalonFX(1);
   private final PositionVoltage m_PositionVoltage = new PositionVoltage(0).withSlot(0).withFeedForward(0);
 
+  private final DigitalInput m_forwardLimit = new DigitalInput(0);
+  private final DigitalInput m_reverseLimit = new DigitalInput(1);
+
   public Climbing() {
+
     m_rightMotor.setControl(new Follower(m_leftMotor.getDeviceID(), true));
+
   }
 
+  // TODO: add the correct constant for the position
   public Command climb() {
-    return run(() -> {setControl(m_PositionVoltage.withPosition(ClimbingConstants.ROTATIONS_FOR_CLIMB));});
+    return run(() -> {setControl(m_PositionVoltage.withPosition(ClimbingConstants.ROTATIONS_FOR_CLIMB).withLimitForwardMotion(m_forwardLimit.get()).withLimitReverseMotion(m_reverseLimit.get()));});
   }
 
   public Command unClimb() {
-    return run(() -> {setControl(m_PositionVoltage.withPosition(0));});
+    return run(() -> {setControl(m_PositionVoltage.withPosition(0).withLimitForwardMotion(m_forwardLimit.get()).withLimitReverseMotion(m_reverseLimit.get()));});
+
   }
 
   public void setControl(ControlRequest control){
